677885373f7fa312f9db569f320cedf504063c44
Prabhat <83655197+Prabhatshaw5@users.noreply.github.com>
2026-07-28 21:34:15 +0530

    Add SQL queries for student categorization and filtering

--- School_db/Solved Answers.sql
+++ School_db/Solved Answers.sql
@@ -37,3 +37,28 @@ from student_raw_transformed_withtime;
 -- categorize the students as Mtest>=95 then scholar or dumb
 select id,fullname, if(MTest>=95,'scholar','dumb') as status
 from student_raw_transformed_withtime;
+
+-- /*1. List the students who come from districts that contain Y in the district name*/
+select FullName, DCode
+from student_raw_transformed_withtime
+where Dcode like "%Y%";
+
+-- 2 List the students who are playing instruments that contain letter "p" somewherg.
+select s.FullName, s.id, m.type
+From student_raw_transformed_withtime s inner join music m on s.id = m.id
+where m.type like "%p%";
+
+-- 3.List the instruments played by students whose names start with letter capital "A".*/
+select s.FullName, m.Type,s.id
+from student_raw_transformed_withtime s inner join music m on s.id = m.id
+where binary s.FullName like "A%";
+
+-- 4. List the students whose name contain small case "u" as the second letter.
+select distinct FullName
+from student_raw_transformed_withtime
+where binary FullName like "_u%";
+
+/*5. List the students whose name contain "u" or "r" somewhere*/
+select FullName
+from student_raw_transformed_withtime
+where FullName like "%u%" or FullName like "%r%";
