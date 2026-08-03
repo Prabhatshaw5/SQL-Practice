cb798a09f4485415c66fff5eaced87a2cc5f1b0b
Prabhat <83655197+Prabhatshaw5@users.noreply.github.com>
2026-08-03 19:05:23 +0530

    Update Web Analytics Questions.sql

--- WebAnalysis/Web Analytics Questions.sql
+++ WebAnalysis/Web Analytics Questions.sql
@@ -15,14 +15,4 @@ from visitdata
 order by internet_usage desc /here order by will sort the data by Internet_Usage in larges tt to smalled values/
 limit 3; /limit 3 will ensure it will only show the top 3 internet_usage visits/
 
-3. List the top 5 richest visitors from Singapore. (Hint: try to use Order By and Limit Clauses)
-4. List the 3 countries with the lowest Avg_income. 
-5. Show all the visits where the internet_usage was more than 230 MB 
-and time_spent was more than 60. Also, show the day of the week for these visits.
-6. Are there any female visitors from any of the following countries?
-- India
-- Pakistan
-- Sri Lanka
-- Fiji
-If there are, please list their visit Id, Avg Income, and DOB.
-*/
+
