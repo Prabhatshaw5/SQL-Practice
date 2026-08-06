6d09a8c7910a66e5a6647d501e23c2e163acdd7f
Prabhat <83655197+Prabhatshaw5@users.noreply.github.com>
2026-08-06 16:03:15 +0530

    Add files via upload

---
+++ Text Functions/Text Func Questions.sql
@@ -0,0 +1,13 @@
+Use School Database (student table) to solve the following questions:
+Text Function Queries: 
+
+Please refer to the attached notes on Text Functions in SQL. Then, please do the following questions using the School_Db: 
+
+1. List the first initial of all the students coming from YMT
+ 2. Display the entire name but the last character of students
+3. Display the last two characters of the students’ name 
+4. Do the second question using Mid function
+ 5. Do the third question using Mid function
+6. Show the fullname in Upper Case. 
+ 7. Select students whose name contain a single “e”.  
+ 8. Find the position of second “e” in fullnames
